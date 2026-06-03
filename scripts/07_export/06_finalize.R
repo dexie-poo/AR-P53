36f2bae6965f3065aa68cd7b043ce39205548070
Dexter <dexter.leung@icloud.com>
2026-06-03 14:15:03 -0400

    Update 06_finalize.R

--- scripts/07_export/06_finalize.R
+++ scripts/07_export/06_finalize.R
@@ -2,11 +2,11 @@
 
 source("scripts/00_setup/00_config.R")
 
-obj <- readRDS(file.path(out_dir, "A1281_epi_sub_reannotated_visualization_complete.rds"))
+obj <- readRDS(file.path(rds_dir, "05_visualization_complete.rds"))
 
 saveRDS(
   obj,
-  file.path(out_dir, "A1281_final_processed.rds")
+  file.path(rds_dir, "06_final_processed.rds")
 )
 
 writeLines(
